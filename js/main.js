cd48d87592ab7af5979ca19aee130fbfeb1b8dcd
AndreaBeltramin <andreabeltra99@gmail.com>
2024-10-22 16:41:08 +0200

    aggiunto seconda foto

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const Immagini = document.getElementById("immagini");
+
+//creo un tag img con un'altra foto
+const secondImg = document.createElement("img");
+secondImg.id = "secondaFoto";
+secondImg.src = "./img/yellow_lamp.png";
+secondImg.alt = "lampadinaaccesa";
+
+console.log(secondImg);
+
+//aggiungo la seconda immagine al div immagini nell'html
+Immagini.append(secondImg);
+
+const firstButton = document.getElementById("comando");
+const primaFoto = document.getElementById("primaFoto");
+
+//scrivo una funzione per scambiare le immagini al click del bottone
+firstButton.addEventListener("click", () => {
+	primaFoto.toogle("seconda foto");
+});
