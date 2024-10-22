789a2ccfcf9b693715ee39e449a11b80c59f1985
AndreaBeltramin <andreabeltra99@gmail.com>
2024-10-22 17:52:47 +0200

    fix posizione bottone + lampadina accesa

--- js/main.js
+++ js/main.js
@@ -6,16 +6,14 @@ secondImg.id = "secondaFoto";
 secondImg.src = "./img/yellow_lamp.png";
 secondImg.alt = "lampadinaaccesa";
 
-console.log(secondImg);
-
-const firstButton = document.getElementById("comando");
-console.log(firstButton);
+const button = document.getElementById("comando");
+console.log(button);
 
 const primaFoto = document.getElementById("primaFoto");
 console.log(primaFoto);
 
 //aggiungo la seconda immagine al div immagini nell'html
-firstButton.addEventListener("click", () => {
+button.addEventListener("click", function () {
 	listaImmagini.append(secondImg);
 	primaFoto.replaceWith(secondImg);
 });
