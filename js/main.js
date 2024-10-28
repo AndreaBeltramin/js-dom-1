ede84526c818920e727989fedca8ecacb0374bbf
AndreaBeltramin <andreabeltra99@gmail.com>
2024-10-28 12:36:14 +0100

    correzione  struttura+ modifiche+ bonus

--- js/main.js
+++ js/main.js
@@ -1,19 +1,42 @@
+//prendo gli elementi che mi servono dall'html
 const listaImmagini = document.getElementById("listaImmagini");
+const primaFoto = document.getElementById("primaFoto");
 
 //creo un tag img con un'altra foto
-const secondImg = document.createElement("img");
-secondImg.id = "secondaFoto";
-secondImg.src = "./img/yellow_lamp.png";
-secondImg.alt = "lampadinaaccesa";
+const secondaFoto = document.createElement("img");
 
+//setto gli attributi di img
+secondaFoto.setAttribute("id", "secondaFoto");
+secondaFoto.setAttribute("src", "./img/yellow_lamp.png");
+secondaFoto.setAttribute("class", "d-none");
+
+//inserisco nell'html il nuovo elemento come figlio di listaImmagini
+listaImmagini.appendChild(secondaFoto);
+
+//richiamo il bottone
 const button = document.getElementById("comando");
 console.log(button);
 
-const primaFoto = document.getElementById("primaFoto");
-console.log(primaFoto);
+const spegni = () => {
+	primaFoto.classList.remove("d-none");
+	secondaFoto.classList.add("d-none");
+	button.innerHTML = "Accendi";
+};
 
-//aggiungo la seconda immagine al div immagini nell'html
-button.addEventListener("click", function () {
-	listaImmagini.append(secondImg);
-	primaFoto.replaceWith(secondImg);
-});
+const accendi = () => {
+	primaFoto.classList.add("d-none");
+	secondaFoto.classList.remove("d-none");
+	button.innerHTML = "Spegni";
+};
+
+const switchLight = () => {
+	if (button.innerHTML === "Accendi") {
+		accendi();
+	} else {
+		spegni();
+	}
+};
+
+//con un click del bottone tolgo la classe d-none alla seconda immagine
+//per poterla vedere e la aggiungo alla prima immagine per poterla nascondere
+button.addEventListener("click", switchLight);
